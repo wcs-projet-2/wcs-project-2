0b7f9777e5bab042111185ffcc1f219f99fc51b3
Marlene Angouillant <marlene.angouillant@gmail.com>
2019-04-02 15:27:14 +0200

    create main components

---
+++ src/bottomBar.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+const BottomBar = () => {
+  return (
+    <header>
+      <p>Bottom Bar</p>
+    </header>
+  );
+};
+
+export default BottomBar;
---
+++ src/content.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+const Content = () => {
+  return (
+    <div>
+      <p>content</p>
+    </div>
+  );
+};
+
+export default Content;
