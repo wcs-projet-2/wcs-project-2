37cddbeac75aa82af2ee49f5a66135d4c2a8c5ce
Marlene Angouillant <marlene.angouillant@gmail.com>
2019-04-04 17:24:00 +0200

    integration liens avec content pour nom de source, icone de source, et emplacement article en grid column

--- src/Components/source.js
+++ src/Components/source.js
@@ -1,38 +1,38 @@
 import React, { Component } from 'react';
+// import Article from './article';
 import { Grid, Container, Header, Icon } from 'semantic-ui-react';
 import './source.css';
 
 class Source extends Component {
   render() {
+    let title = this.props.source;
+    let iconName = this.props.source;
+    let iconColor;
+
+    if (iconName === 'Twitter') {
+      iconColor = 'blue';
+    } else if (iconName === 'Reddit') {
+      iconColor = 'red';
+    } else if (iconName === 'Hacker') {
+      iconColor = 'green';
+    }
+
     return (
       <div>
         <Grid>
           <Grid.Row>
-            <Container fluid>
-              <Header as="h3" textAlign="left">
-                {' '}
-                Twitter <Icon name="twitter" color="blue" />{' '}
-              </Header>
-              <p>Cards</p>
-            </Container>
+            {/* <Container fluid> */}
+            <Header as="h3" textAlign="left">
+              <Icon name={iconName} color={iconColor} />
+            </Header>
+            <p> {title} </p>
           </Grid.Row>
           <Grid.Row>
-            <Container fluid>
-              <Header as="h3" textAlign="left">
-                {' '}
-                Reddit <Icon name="reddit alien" color="red" />{' '}
-              </Header>
-              <p>Cards</p>
-            </Container>
-          </Grid.Row>
-          <Grid.Row>
-            <Container fluid>
-              <Header as="h3" textAlign="left">
-                {' '}
-                The Hacker news <Icon name="hacker news" color="green" />{' '}
-              </Header>
-              <p>Cards</p>
-            </Container>
+            <Grid.Column width={3}>Article HERE</Grid.Column>
+            <Grid.Column width={3}>Article HERE</Grid.Column>
+            <Grid.Column width={3}>Article HERE</Grid.Column>
+            <Grid.Column width={3}>Article HERE</Grid.Column>
+            {/* </Container> */}
           </Grid.Row>
         </Grid>
       </div>
