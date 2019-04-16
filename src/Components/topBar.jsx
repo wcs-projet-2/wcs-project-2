2a727afa574c2d9503a5a07bb0e816edfef2d358
Amine BEN TALEB <amine.bentaleb@gmx.fr>
2019-04-16 11:26:46 +0200

    passing state to checkboxes checkboxes value is modified according to the state when moving from home to search page

--- src/Components/topBar.jsx
+++ src/Components/topBar.jsx
@@ -18,7 +18,7 @@ const TopBar = (props) => {
           <Input
             className="icon"
             placeholder="Search..."
-            icon={{ name: 'search', circular: true, link: true, onClick: () => props.clickHandler() }}
+            icon={{ name: 'search', circular: props.toggle, link: true, onClick: () => props.clickHandler() }}
             value={props.searchValue}
             onChange={props.changeHandler}
             onKeyPress={(event) => {
@@ -29,15 +29,15 @@ const TopBar = (props) => {
           />
           <div>
             <Icon className="twitter" />
-            <Checkbox toggle defaultChecked={true} onChange={() => props.onToggle('twitter')} />
+            <Checkbox toggle checked={props.toggle.twitter} onChange={() => props.onToggle('twitter')} />
           </div>
           <div>
             <Icon className="reddit alien" />
-            <Checkbox toggle defaultChecked={true} onChange={() => props.onToggle('reddit')} />
+            <Checkbox toggle checked={props.toggle.reddit} onChange={() => props.onToggle('reddit')} />
           </div>
           <div>
             <Icon className="moon" />
-            <Checkbox toggle defaultChecked={true} onChange={() => props.onToggle('hacker')} />
+            <Checkbox toggle checked={props.toggle.hacker} onChange={() => props.onToggle('hacker')} />
           </div>
         </Menu.Item>
       </Menu>
